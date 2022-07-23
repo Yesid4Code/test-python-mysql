dcc9d9f5a57f966480f2ca6469ad45f5e10d54d3
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 14:04:30 -0500

    query of a branch with the id

--- test-python/test_2.py
+++ test-python/test_2.py
@@ -3,3 +3,22 @@
   consultar las sucursales por su id, debe hacerse el mismo procedimiento
   que se realizó en el punto 1, pero, utilizando la función creada
 """
+from data import Data
+
+
+def get_branche(id: int) -> dict:
+  """Function that gets the information of a branch"""
+  return (Data().get_branches()[id])
+
+
+companies = Data.get_companies()
+
+
+for companie in companies:
+    branches_name = []
+    for branch in companie["branches"]:  # Get branches of each companie
+        branches_name.append(get_branche(branch - 1))
+
+    companie["branches_name"] = branches_name
+
+print(companies)
