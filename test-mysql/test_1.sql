777540927edf256a41053495c1be7512d08e9886
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 11:13:27 -0500

    basic queries added

--- test-mysql/test_1.sql
+++ test-mysql/test_1.sql
@@ -2,12 +2,13 @@
 
 -------* Consultar los items que pertenezcan a la compañia (utilizando INNER JOIN) con ID #3 *-------
 
+SELECT * FROM items INNER JOIN companies ON items.companyId = companies.id WHERE companies.id = 3;
 
 -------* Mostrar los últimos 10 items *-------
-
+SELECT * FROM items ORDER BY id DESC LIMIT 10;
 
 -------* Mostrar los items que en el nombre terminen con la letra A *-------
-
+SELECT * FROM items ORDER BY id DESC LIMIT 10;
 
 -------* Mostrar los items que tengan relacionado el color Rojo *-------
-
+SELECT * FROM items INNER JOIN colors ON items.colorid = colors.id WHERE colors.name = "ROJO";
