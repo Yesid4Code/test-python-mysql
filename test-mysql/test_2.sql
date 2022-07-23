1741c5580627974c7c35c0d9ccb32d7d946412e9
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 11:30:20 -0500

    test2

--- test-mysql/test_2.sql
+++ test-mysql/test_2.sql
@@ -2,9 +2,10 @@
 
 -------* Colocarle el Precio a los items que lo tengan en NULL, tomando como referencia el valor del costo del item + 10.000 *-------
 
+UPDATE items SET price = 10000 WHERE price = 0
 
 -------* Incrementar el precio de los items en 10% *-------
-
+UPDATE items SET price = (price + (price * 0.1));
 
 -------* Anteponer la palabra Nuevo a los items que comiencen con C en el nombre *-------
-
+UPDATE items SET name = CONCAT("Nuevo", " ", name) WHERE name LIKE 'C%';
