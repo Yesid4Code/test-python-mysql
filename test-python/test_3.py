43884d3e8dc62e396f0ceeb147b3cbed46293d4e
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 15:41:43 -0500

    Thirds order by tradename

--- test-python/test_3.py
+++ test-python/test_3.py
@@ -1,9 +1,31 @@
 """
-  3) ordenar los terceros que se tienen en el archivo data.py 
-  por nombre, para obtener el nombre correcto se debe tener en 
+  3) ordenar los terceros que se tienen en el archivo data.py
+  por nombre, para obtener el nombre correcto se debe tener en
   cuenta la siguiente validación:
   
-  si el tercero tiene un (tradename != '') entonces se muestra este valor, 
-  en caso contrario se debe obtener concatenando los siguientes 
+  si el tercero tiene un (tradename != '') entonces se muestra este valor,
+  en caso contrario se debe obtener concatenando los siguientes
   campos: (firstname, lastname, maidenname) en el orden dado
 """
+from data import Data
+
+thirds = Data.get_thirds()
+
+for third in thirds:
+    if third["tradename"] == "":
+        third["tradename"] = third["firstname"] + " " + third["lastname"] + " " + third["maidenname"]
+
+
+def myFunc(e):
+    """Function that return 'tradename' as the value"""
+    return e["tradename"]
+
+def print_trade(thirds):
+    for third in thirds:
+        print("tradename: ", third["tradename"])
+
+
+print_trade(thirds)
+thirds.sort(key=myFunc)  # sorted by tradename
+print("--------------- Sorted ---------------")
+print_trade(thirds)
