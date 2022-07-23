bf7289b64acf83463c2fe4f5c192b978052df76e
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 13:46:13 -0500

    Query of the company with its respective branches

--- test-python/test_1.py
+++ test-python/test_1.py
@@ -1,6 +1,20 @@
-""" 
+"""
   1) consulte la información del archivo data.py
   cree un objeto que contenga las empresas y dentro 
   las sucursales que corresponden para cada empresa
 """
+from data import Data
 
+
+companies = Data.get_companies()
+
+
+for companie in companies:
+    branches_name = []
+    for branch in companie["branches"]:  # Get branches of each companie
+        get_branch_name = Data().get_branches()[branch - 1]
+        branches_name.append(get_branch_name)
+
+    companie["branches_name"] = branches_name
+
+print(companies)
