6b99a621a5e2a01635e5756207aaa248588c82d9
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-07-23 15:52:21 -0500

    Thirds order by 'identificationNumber'

--- test-python/test_4.py
+++ test-python/test_4.py
@@ -1,3 +1,25 @@
 """
   4) ordenar los terceros que se tienen en el archivo data.py por identificationNumber
 """
+from data import Data
+
+
+thirds = Data.get_thirds()
+
+
+def myFunc(e):
+    """Function that return 'tradename' as the value"""
+    return e["identificationNumber"]
+
+
+def print_identificationNumber(thirds):
+    for third in thirds:
+        print("tradename: ", third["identificationNumber"])
+
+
+print_identificationNumber(thirds)
+
+thirds.sort(key=myFunc)  # sorted by identificationNumber
+
+print("--------------- Sorted ---------------")
+print_identificationNumber(thirds)
